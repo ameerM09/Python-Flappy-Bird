8a4119974184fa54243fc118bb650ed9a73e25fb
Ameer Mohammad <96792013+ameerM09@users.noreply.github.com>
2023-05-15 17:59:00 -0700

    Add files via upload

---
+++ settings/__init__.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import sys
+import os
+
+pygame.init()
+pygame.font.init()
+
+# Relative imports of other package modules
+from .bird import *
+from .obstacle import *
+from .platform import *
